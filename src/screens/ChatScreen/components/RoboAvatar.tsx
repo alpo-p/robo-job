95bd69309519d37f52d9d8ed6f9aa59ea4affc3f
alpo-p <alpo.panula@gmail.com>
2022-01-08 13:56:15 +0200

    fix roboavatar style

--- src/screens/ChatScreen/components/RoboAvatar.tsx
+++ src/screens/ChatScreen/components/RoboAvatar.tsx
@@ -1,4 +1,3 @@
-import { useTheme } from '@react-navigation/native'
 import React from 'react'
 import { View } from 'react-native'
 import SmallCompanyLogo from '../../ChatRowsScreen/components/SmallCompanyLogo'
@@ -7,12 +6,11 @@ const ROBO_COP_URL =
   'https://cdn3.iconfinder.com/data/icons/movies-solid/60/54_-_Robocop_video_film_cinema_movie-1024.png'
 
 export default () => {
-  const { colors } = useTheme()
   const SIZE = 32
   return (
     <View
       style={{
-        backgroundColor: colors.background,
+        backgroundColor: 'white',
         width: SIZE,
         height: SIZE,
         borderRadius: SIZE / 2,
