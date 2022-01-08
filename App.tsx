77a79a90240e85c60c4675263c293c57783778a7
alpo-p <alpo.panula@gmail.com>
2022-01-08 09:47:06 +0200

    safeareaprovider

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { LogBox, StatusBar } from 'react-native'
 import { initializeApp } from 'firebase/app'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import Navigator from './src/Navigator'
 import AuthStorage from './src/utils/authStorage'
 
@@ -22,11 +23,13 @@ const App: React.FC = () => {
   return (
     <>
       <StatusBar backgroundColor="transparent" translucent />
-      <LikedJobsProvider>
-        <AuthStorageContext.Provider value={authStorage}>
-          <Navigator />
-        </AuthStorageContext.Provider>
-      </LikedJobsProvider>
+      <SafeAreaProvider>
+        <LikedJobsProvider>
+          <AuthStorageContext.Provider value={authStorage}>
+            <Navigator />
+          </AuthStorageContext.Provider>
+        </LikedJobsProvider>
+      </SafeAreaProvider>
     </>
   )
 }
